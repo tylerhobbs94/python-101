5475c02f1a6f12a9fbd2a95a3a3c62d0a225628a
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-01-13 10:56:40 -0500

    fixed days-of-the-week

--- days-of-the-week.py
+++ days-of-the-week.py
@@ -1,2 +1,22 @@
 day = int(input('day (0-6)? '))
-print" is first day of the week'
+
+if day == 0:
+    print("the day is Sunday.")
+
+elif day == 1:
+    print ("The day is Monday")
+
+elif day == 2:
+    print("The day is Tuesday")    
+
+elif day == 3:
+    print("The day is Wednesday")
+
+elif day == 4:
+    print("The day is Thursday")
+
+elif day == 5:
+    print("The day is Friday")
+
+elif day == 6:
+    print("The day is Saturday")
