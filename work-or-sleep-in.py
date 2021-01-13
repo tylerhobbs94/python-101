9602ce16204666e34b32b37babed8d561f6c6b4a
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-01-13 11:16:38 -0500

    fixed work-or-sleep-.py

---
+++ work-or-sleep-in.py
@@ -0,0 +1,18 @@
+day = int(input("day 0-6?"))
+
+if day == 0:
+    print("Go to work.")
+
+elif day == 1:
+    print("Go to work.")
+elif day == 2:
+    print("Go to work.")
+elif day == 3:
+    print("Go to work.")
+elif day == 4:
+    print("Go to work.")
+elif day == 5:
+    print("Go to work.")
+elif day == 6:
+    print("Sleep in.")
+
