dd29ce4f61ba0cebe7323fbc4fd4da8cacb27cdc
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-01-13 12:16:46 -0500

    finished exercise n-to-m.py

---
+++ looping-from-1-to-10.py
@@ -0,0 +1,4 @@
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
---
+++ n-to-m.py
@@ -0,0 +1,7 @@
+start = int(input("tell the user to start at S"))
+end = int(input("tell the user to end at E"))
+
+
+while start <= end:
+    print(start)
+    start += 1
