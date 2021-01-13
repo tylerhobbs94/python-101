7044eaa48dac757c88bf0cbf3d57c909830abb96
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-01-13 14:40:09 -0500

    created print-a-square

--- Celsius-to-fahrenheit.py
+++ Celsius-to-fahrenheit.py
@@ -1,5 +1,10 @@
-c = int(input("what is the temperature in Celsius?"))
+while(True):
+    try:
+        c = int(input("what is the temperature in Celsius?"))
 
-F = (c *9/5) + 32
+        F = (c *9/5) + 32
 
-print("The temperature in C is %s" %F)
+        print("The temperature in C is %s" %F)
+        break
+    except ValueError:
+        print("please insert a number.")
